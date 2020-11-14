0c8b427df95be7c96ef0293024adb46cd37e7fb9
DESKTOP-4COC7HN\Walter Guzman <guillememo95@gmail.com>
2020-11-13 18:52:30 -0600

    Initial commit

---
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/DevicesFragment.java
@@ -0,0 +1,131 @@
+package de.kai_morich.simple_bluetooth_terminal;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DevicesFragment extends ListFragment {
+
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayList<BluetoothDevice> listItems = new ArrayList<>();
+    private ArrayAdapter<BluetoothDevice> listAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        if(getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH))
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        listAdapter = new ArrayAdapter<BluetoothDevice>(getActivity(), 0, listItems) {
+            //vista de cada item de los dispositivos de bluetooh reconocidos por el cel
+            @Override
+            public View getView(int position, View view, ViewGroup parent) {
+                BluetoothDevice device = listItems.get(position);
+                if (view == null)
+                    view = getActivity().getLayoutInflater().inflate(R.layout.device_list_item, parent, false);
+                TextView text1 = view.findViewById(R.id.text1);
+                TextView text2 = view.findViewById(R.id.text2);
+                text1.setText(device.getName());
+                text2.setText(device.getAddress());
+                return view;
+            }
+        };
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setListAdapter(null);
+        View header = getActivity().getLayoutInflater().inflate(R.layout.device_list_header, null, false);
+        getListView().addHeaderView(header, null, false);
+        setEmptyText("initializing...");
+        ((TextView) getListView().getEmptyView()).setTextSize(18);
+        setListAdapter(listAdapter);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_devices, menu);
+        if(bluetoothAdapter == null)
+            menu.findItem(R.id.bt_settings).setEnabled(false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(bluetoothAdapter == null)
+            setEmptyText("bluetooth no soportado en el dispositivo");
+        else if(!bluetoothAdapter.isEnabled())
+            setEmptyText("bluetooth esta desconectado");
+        else
+            setEmptyText("no funciona el bluetooh");
+        refresh();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.bt_settings) {
+            Intent intent = new Intent();
+            //para lanzar la configuracion de bluetooh de nuestro dispositivo
+            intent.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    void refresh() {
+        listItems.clear();
+        if(bluetoothAdapter != null) {
+            for (BluetoothDevice device : bluetoothAdapter.getBondedDevices())
+                if (device.getType() != BluetoothDevice.DEVICE_TYPE_LE)
+                    //lenamos la lista de los dispositivos conectados a blueetooh en el dispositivo
+                    listItems.add(device);
+        }
+        Collections.sort(listItems, DevicesFragment::compareTo);
+        listAdapter.notifyDataSetChanged();
+    }
+    //listener de cuando elegimos el dispositivo bluetooh a utilizar
+    //cada item de bluetooh , cuando pulsamos sobre uno de ellos para usarlo en la app
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+
+        BluetoothDevice device = listItems.get(position-1);
+        Bundle args = new Bundle();
+        args.putString("device", device.getAddress());
+        Fragment fragment = new TerminalFragment();
+        fragment.setArguments(args);
+        getFragmentManager().beginTransaction().replace(R.id.fragment, fragment, "terminal").addToBackStack(null).commit();
+    }
+
+    //con esto ordenamos los items que mostraremos de los dispositivos bluetooh
+    static int compareTo(BluetoothDevice a, BluetoothDevice b) {
+        boolean aValid = a.getName()!=null && !a.getName().isEmpty();
+        boolean bValid = b.getName()!=null && !b.getName().isEmpty();
+        if(aValid && bValid) {
+            int ret = a.getName().compareTo(b.getName());
+            if (ret != 0) return ret;
+            return a.getAddress().compareTo(b.getAddress());
+        }
+        if(aValid) return -1;
+        if(bValid) return +1;
+        return a.getAddress().compareTo(b.getAddress());
+    }
+}
---
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/TerminalFragment.java
@@ -0,0 +1,291 @@
+package de.kai_morich.simple_bluetooth_terminal;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.method.ScrollingMovementMethod;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TerminalFragment extends Fragment implements ServiceConnection, /*esta es la interface que creamos*/ SerialListener {
+
+    private enum Connected { False, Pending, True }
+
+    private String deviceAddress;
+    private SerialService service;
+
+    private TextView receiveText;
+    private TextView sendText;
+    private TextUtil.HexWatcher hexWatcher;
+
+    private Connected connected = Connected.False;
+    private boolean initialStart = true;
+    private boolean hexEnabled = false;
+    private boolean pendingNewline = false;
+    private String newline = TextUtil.newline_crlf;
+
+    /*
+     * Lifecycle
+     */
+    //primero
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        setRetainInstance(true);
+        deviceAddress = getArguments().getString("device");
+
+    }
+
+    @Override
+    public void onDestroy() {
+        if (connected != Connected.False)
+            disconnect();
+        getActivity().stopService(new Intent(getActivity(), SerialService.class));
+        super.onDestroy();
+    }
+    //segundo
+    //aqui realizamos la conexion con la clase serialservice y emparejamos con bluetooh
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(service != null)
+            service.attach(this);
+        else
+            getActivity().startService(new Intent(getActivity(), SerialService.class));
+    }
+
+    @Override
+    public void onStop() {
+        if(service != null && !getActivity().isChangingConfigurations())
+            service.detach();
+        super.onStop();
+    }
+
+    @SuppressWarnings("deprecation") // onAttach(context) was added with API 23. onAttach(activity) works for all API versions
+    @Override
+    public void onAttach(@NonNull Activity activity) {
+        super.onAttach(activity);
+        getActivity().bindService(new Intent(getActivity(), SerialService.class), this, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onDetach() {
+        try { getActivity().unbindService(this); } catch(Exception ignored) {}
+        super.onDetach();
+    }
+    //tercero
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(initialStart && service != null) {
+            initialStart = false;
+            getActivity().runOnUiThread(this::connect);
+        }
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder binder) {
+        service = ((SerialService.SerialBinder) binder).getService();
+        service.attach(this);
+        if(initialStart && isResumed()) {
+            initialStart = false;
+            getActivity().runOnUiThread(this::connect);
+        }
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        service = null;
+    }
+
+    /*
+     * UI
+     */
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_terminal, container, false);
+        //texto recibimos si esta conectado
+        /*
+        receiveText = view.findViewById(R.id.receive_text);
+        receiveText.setTextColor(getResources().getColor(R.color.colorRecieveText));
+        receiveText.setMovementMethod(ScrollingMovementMethod.getInstance());
+        //
+        sendText = view.findViewById(R.id.send_text);
+        //hexWatcher = new TextUtil.HexWatcher(sendText);
+        //hexWatcher.enable(hexEnabled);
+        //sendText.addTextChangedListener(hexWatcher);
+        //sendText.setHint(hexEnabled ? "HEX mode" : "");
+*/
+        View btn1 = view.findViewById(R.id.button);
+        View btn2 = view.findViewById(R.id.button2);
+        btn1.setOnClickListener(v -> send("1"));
+        btn2.setOnClickListener(v -> send("2"));
+        return view;
+    }
+
+    /*@Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_terminal, menu);
+        menu.findItem(R.id.hex).setChecked(hexEnabled);
+    }*/
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.clear) {
+            receiveText.setText("");
+            return true;
+        } else if (id == R.id.newline) {
+            String[] newlineNames = getResources().getStringArray(R.array.newline_names);
+            String[] newlineValues = getResources().getStringArray(R.array.newline_values);
+            int pos = java.util.Arrays.asList(newlineValues).indexOf(newline);
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Newline");
+            builder.setSingleChoiceItems(newlineNames, pos, (dialog, item1) -> {
+                newline = newlineValues[item1];
+                dialog.dismiss();
+            });
+            builder.create().show();
+            return true;
+        } else if (id == R.id.hex) {
+            hexEnabled = !hexEnabled;
+            sendText.setText("");
+            hexWatcher.enable(hexEnabled);
+            sendText.setHint(hexEnabled ? "HEX mode" : "");
+            item.setChecked(hexEnabled);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }*/
+
+
+    /*
+     * Serial + UI
+     */
+    private void connect() {
+        try {
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+            //status("connecting...");
+
+            connected = Connected.Pending;
+            SerialSocket socket = new SerialSocket(getActivity().getApplicationContext(), device);
+            service.connect(socket);
+        } catch (Exception e) {
+            onSerialConnectError(e);
+        }
+    }
+
+    private void disconnect() {
+        connected = Connected.False;
+        service.disconnect();
+    }
+
+    private void send(String str) {
+       // if(connected != Connected.True) {
+       //     Toast.makeText(getActivity(), "no estas conectado correctamente", Toast.LENGTH_SHORT).show();
+       //     return;
+       // }
+        try {
+            String msg;
+            byte[] data;
+            if(hexEnabled) {
+                StringBuilder sb = new StringBuilder();
+                TextUtil.toHexString(sb, TextUtil.fromHexString(str));
+                TextUtil.toHexString(sb, newline.getBytes());
+                msg = sb.toString();
+                data = TextUtil.fromHexString(msg);
+            } else {
+                msg = str;
+                data = (str + newline).getBytes();
+            }
+            //SpannableStringBuilder spn = new SpannableStringBuilder(msg+'\n');
+            //spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorSendText)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            //receiveText.append(spn);
+            //llamamos al servicio para hacer el envio
+
+            service.write(data);
+        } catch (Exception e) {
+            onSerialIoError(e);
+        }
+    }
+
+    private void receive(byte[] data) {
+        if(hexEnabled) {
+            receiveText.append(TextUtil.toHexString(data) + '\n');
+        } else {
+            String msg = new String(data);
+            if(newline.equals(TextUtil.newline_crlf) && msg.length() > 0) {
+                // don't show CR as ^M if directly before LF
+                msg = msg.replace(TextUtil.newline_crlf, TextUtil.newline_lf);
+                // special handling if CR and LF come in separate fragments
+                if (pendingNewline && msg.charAt(0) == '\n') {
+                    Editable edt = receiveText.getEditableText();
+                    if (edt != null && edt.length() > 1)
+                        edt.replace(edt.length() - 2, edt.length(), "");
+                }
+                pendingNewline = msg.charAt(msg.length() - 1) == '\r';
+            }
+            receiveText.append(TextUtil.toCaretString(msg, newline.length() != 0));
+        }
+    }
+
+
+
+    /*
+     * SerialListener
+     */
+
+    @Override
+    public void onSerialConnect() {
+        //status("connected");
+        Toast.makeText(getActivity(),"serial conectado ",Toast.LENGTH_LONG).show();
+
+        connected = Connected.True;
+    }
+
+    @Override
+    public void onSerialConnectError(Exception e) {
+        //status("connection failed: " + e.getMessage());
+        Toast.makeText(getActivity(),"serial desconectado "+e.getMessage(),Toast.LENGTH_LONG).show();
+        disconnect();
+    }
+
+    @Override
+    public void onSerialRead(byte[] data) {
+        receive(data);
+    }
+
+    @Override
+    public void onSerialIoError(Exception e) {
+        //status("connection lost: " + e.getMessage());
+        Toast.makeText(getActivity(),"desconectado en la salida",Toast.LENGTH_LONG).show();
+
+        disconnect();
+    }
+
+}
